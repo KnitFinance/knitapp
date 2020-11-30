c0d47048f53a37ebab2638257388112108624a62
Nikhil M <nikhil@lightrains.com>
2020-11-30 18:13:38 +0530

    Add withdraw page

---
+++ src/Withdraw.js
@@ -0,0 +1,61 @@
+import * as React from 'react'
+import {
+    Container,
+    Header,
+    Divider,
+    Input,
+    Dropdown,
+    Button,
+    Form,
+    Checkbox
+} from 'semantic-ui-react'
+
+const options = [
+    { key: 'kxlm', text: 'K-XLM', value: 'kxlm' },
+    { key: 'kxrp', text: 'K-XRP', value: 'kxrp' },
+    { key: 'kltc', text: 'K-LTC', value: 'kltc' }
+]
+function Withdraw() {
+    return (
+        <React.Fragment>
+            <Divider hidden />
+            <Header as="h2">Withdraw</Header>
+            <Form>
+                <Form.Field>
+                    <Input
+                        label={
+                            <Dropdown
+                                defaultValue="kxlm"
+                                size="huge"
+                                options={options}
+                            />
+                        }
+                        labelPosition="right"
+                        size="huge"
+                        placeholder="Enter Wallet Address"
+                    />
+                </Form.Field>
+                <Form.Field>
+                    <Input size="huge" placeholder="Amount to withdraw" />
+                </Form.Field>
+
+                <Form.Field>
+                    <Checkbox label="I agree to the terms and privacy policy" />
+                </Form.Field>
+
+                <Form.Field>
+                    <div>
+                        <Button primary size="huge">
+                            Withdraw
+                        </Button>
+                        <Button secondary size="huge">
+                            RESET
+                        </Button>
+                    </div>
+                </Form.Field>
+            </Form>
+        </React.Fragment>
+    )
+}
+
+export default Withdraw
