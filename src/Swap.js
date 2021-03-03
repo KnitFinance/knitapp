e041f9ec903c841c5ae9b68ecdc959e1efffd605
Sibiraj PR <sibiraj@lightrains.com>
2021-03-03 13:41:09 +0530

    Adding update

--- src/Swap.js
+++ src/Swap.js
@@ -98,126 +98,139 @@ const Swap = () => {
 
     return (
         <React.Fragment>
-            {status && (
-                <Form
-                    inverted
-                    className="centermiddle"
-                    onSubmit={handleSubmit(onSubmitHandler)}
-                    name={'swap'}>
-                    <div className="tab-middle">
-                        <Button.Group>
-                            <Button color="violet" type="button">
-                                Ethereum
-                            </Button>
-                            <Button.Or />
-                            <Button
-                                className="maticbtn"
-                                aria-label="Coming Soon"
-                                data-balloon-pos="up"
-                                type="button">
-                                Matic
-                            </Button>
-                            <Button.Or />
-                            <Button
-                                className="maticbtn"
-                                aria-label="Coming Soon"
-                                data-balloon-pos="up"
-                                type="button">
-                                Polkadot
-                            </Button>
-                        </Button.Group>
-                    </div>
-                    <Divider hidden />
-                    <Divider hidden />
+            <Form
+                inverted
+                className="centermiddle"
+                onSubmit={handleSubmit(onSubmitHandler)}
+                name={'swap'}>
+                <div className="tab-middle">
+                    <Button.Group>
+                        <Button color="violet" type="button">
+                            Ethereum
+                        </Button>
+                        <Button.Or />
+                        <Button
+                            className="maticbtn"
+                            aria-label="Coming Soon"
+                            data-balloon-pos="up"
+                            type="button">
+                            Matic
+                        </Button>
+                        <Button.Or />
+                        <Button
+                            className="maticbtn"
+                            aria-label="Coming Soon"
+                            data-balloon-pos="up"
+                            type="button">
+                            Polkadot
+                        </Button>
+                    </Button.Group>
+                </div>
+                <Divider hidden />
+                <Divider hidden />
 
-                    <Form.Group>
-                        <Controller
-                            control={control}
-                            name="amount"
-                            defaultValue={''}
-                            rules={{ required: true }}
-                            render={({ onChange, onBlur, value, ref }) => (
-                                <Form.Field>
-                                    <Input
-                                        label={
-                                            <Dropdown
-                                                options={options}
-                                                size="huge"
-                                                defaultValue={coin}
-                                                onChange={(e, data) => {
-                                                    setCoin(data.value)
-                                                }}
-                                            />
-                                        }
-                                        onChange={onChange}
-                                        type="number"
-                                        onBlur={e => {
-                                            const commission =
-                                                (e.target.value / 100) * 0.25
-                                            const minimumAmount = 0.0000001
-                                            const numberOfToken =
-                                                e.target.value -
-                                                (commission + minimumAmount)
-
-                                            if (numberOfToken < 0) {
-                                                setError('token', {
-                                                    type: 'manual',
-                                                    message:
-                                                        'Minimum amount required!'
-                                                })
-                                            } else {
-                                                setValue(
-                                                    'token',
-                                                    numberOfToken,
-                                                    {
-                                                        shouldDirty: true
-                                                    }
-                                                )
-                                            }
-                                        }}
-                                        labelPosition="right"
-                                        size="huge"
-                                        value={value}
-                                        placeholder="Sending Amount"
-                                    />
-                                </Form.Field>
-                            )}
-                        />
-                        <Controller
-                            control={control}
-                            name="token"
-                            defaultValue={''}
-                            render={({ onChange, onBlur, value, ref }) => (
-                                <Form.Field>
-                                    <Input
-                                        label={
-                                            <Dropdown
-                                                options={optionsWithdraw}
-                                                size="huge"
-                                                value={coin}
-                                            />
-                                        }
-                                        type="text"
-                                        labelPosition="right"
-                                        size="huge"
-                                        value={`~${value}`}
-                                        placeholder="Receiving Amount"
-                                        readOnly
-                                    />
-                                </Form.Field>
-                            )}
-                        />
-                    </Form.Group>
+                <Form.Group>
                     <Controller
                         control={control}
-                        name="ethWallet"
+                        name="amount"
                         defaultValue={''}
-                        rules={{ required: true, minLength: 10 }}
+                        rules={{ required: true }}
+                        render={({ onChange, onBlur, value, ref }) => (
+                            <Form.Field>
+                                <Input
+                                    label={
+                                        <Dropdown
+                                            options={options}
+                                            size="huge"
+                                            defaultValue={coin}
+                                            onChange={(e, data) => {
+                                                setCoin(data.value)
+                                            }}
+                                        />
+                                    }
+                                    onChange={onChange}
+                                    type="number"
+                                    onBlur={e => {
+                                        const commission =
+                                            (e.target.value / 100) * 0.25
+                                        const minimumAmount = 0.0000001
+                                        const numberOfToken =
+                                            e.target.value -
+                                            (commission + minimumAmount)
+
+                                        if (numberOfToken < 0) {
+                                            setError('token', {
+                                                type: 'manual',
+                                                message:
+                                                    'Minimum amount required!'
+                                            })
+                                        } else {
+                                            setValue('token', numberOfToken, {
+                                                shouldDirty: true
+                                            })
+                                        }
+                                    }}
+                                    labelPosition="right"
+                                    size="huge"
+                                    value={value}
+                                    placeholder="Sending Amount"
+                                />
+                            </Form.Field>
+                        )}
+                    />
+                    <Controller
+                        control={control}
+                        name="token"
+                        defaultValue={''}
+                        render={({ onChange, onBlur, value, ref }) => (
+                            <Form.Field>
+                                <Input
+                                    label={
+                                        <Dropdown
+                                            options={optionsWithdraw}
+                                            size="huge"
+                                            value={coin}
+                                        />
+                                    }
+                                    type="text"
+                                    labelPosition="right"
+                                    size="huge"
+                                    value={`~${value}`}
+                                    placeholder="Receiving Amount"
+                                    readOnly
+                                />
+                            </Form.Field>
+                        )}
+                    />
+                </Form.Group>
+                <Controller
+                    control={control}
+                    name="ethWallet"
+                    defaultValue={''}
+                    rules={{ required: true, minLength: 10 }}
+                    render={({ onChange, onBlur, value, ref }) => (
+                        <Form.Field>
+                            <Input
+                                size="large"
+                                placeholder="Your ETH Address"
+                                onChange={onChange}
+                                onBlur={onBlur}
+                                value={value}
+                            />
+                        </Form.Field>
+                    )}
+                />
+                {coin === 'XLM' && (
+                    <Controller
+                        control={control}
+                        name="memo"
+                        defaultValue={''}
+                        rules={{ required: true }}
                         render={({ onChange, onBlur, value, ref }) => (
                             <Form.Field>
                                 <Input
                                     size="large"
-                                    placeholder="Your ETH Address"
+                                    placeholder="Memo"
                                     onChange={onChange}
                                     onBlur={onBlur}
                                     value={value}
@@ -225,82 +238,63 @@ const Swap = () => {
                             </Form.Field>
                         )}
                     />
-                    {coin === 'XLM' && (
-                        <Controller
-                            control={control}
-                            name="memo"
-                            defaultValue={''}
-                            rules={{ required: true }}
-                            render={({ onChange, onBlur, value, ref }) => (
-                                <Form.Field>
-                                    <Input
-                                        size="large"
-                                        placeholder="Memo"
-                                        onChange={onChange}
-                                        onBlur={onBlur}
-                                        value={value}
-                                    />
-                                </Form.Field>
-                            )}
-                        />
-                    )}
+                )}
 
-                    <Controller
-                        control={control}
-                        name="terms"
-                        rules={{ required: true }}
-                        defaultValue={false}
-                        render={({ onChange, onBlur, value, name, ref }) => (
-                            <Form.Field>
-                                <Checkbox
-                                    onBlur={onBlur}
-                                    onChange={e => onChange(!value)}
-                                    checked={value}
-                                    inputRef={ref}
-                                    label="I agree to the terms and privacy policy"
-                                />
-                            </Form.Field>
+                <Controller
+                    control={control}
+                    name="terms"
+                    rules={{ required: true }}
+                    defaultValue={false}
+                    render={({ onChange, onBlur, value, name, ref }) => (
+                        <Form.Field>
+                            <Checkbox
+                                onBlur={onBlur}
+                                onChange={e => onChange(!value)}
+                                checked={value}
+                                inputRef={ref}
+                                label="I agree to the terms and privacy policy"
+                            />
+                        </Form.Field>
+                    )}
+                />
+
+                {Object.keys(errors).length > 0 && (
+                    <>
+                        {errors.token ? (
+                            <Message
+                                color="red"
+                                inverted
+                                header="There was some errors with your submission"
+                                list={[`${errors.token.message}`]}
+                            />
+                        ) : (
+                            <Message
+                                color="red"
+                                inverted
+                                header="There was some errors with your submission"
+                                list={[`All fields are required!`]}
+                            />
                         )}
-                    />
+                    </>
+                )}
+                <Divider hidden />
+                <Divider hidden />
 
-                    {Object.keys(errors).length > 0 && (
-                        <>
-                            {errors.token ? (
-                                <Message
-                                    color="red"
-                                    inverted
-                                    header="There was some errors with your submission"
-                                    list={[`${errors.token.message}`]}
-                                />
-                            ) : (
-                                <Message
-                                    color="red"
-                                    inverted
-                                    header="There was some errors with your submission"
-                                    list={[`All fields are required!`]}
-                                />
-                            )}
-                        </>
-                    )}
-                    <Divider hidden />
-                    <Divider hidden />
-
-                    <Form.Field>
-                        <div>
-                            <Button
-                                primary
-                                size="huge"
-                                loading={loading}
-                                disabled={status}>
-                                EXCHANGE
-                            </Button>
-                            <Button basic color="red" size="huge" type="reset">
-                                RESET
-                            </Button>
-                        </div>
-                    </Form.Field>
-                </Form>
-            )}
+                <Form.Field>
+                    <div>
+                        <Button
+                            primary
+                            size="huge"
+                            loading={loading}
+                            disabled={status}>
+                            EXCHANGE
+                        </Button>
+                        <Button basic color="red" size="huge" type="reset">
+                            RESET
+                        </Button>
+                    </div>
+                </Form.Field>
+            </Form>
 
             {status && (
                 <>
