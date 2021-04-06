5633abd28410eac0ef8216d823146db9e8a0df0e
Sibiraj PR <sibiraj@lightrains.com>
2021-04-06 10:57:05 +0530

    Adding swap v2

--- src/actions/axios.js
+++ src/actions/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 export default axios.create({
-    //baseURL: 'https://knit.lightrains.com/api',
-    baseURL: 'https://1f5b040e983f.ngrok.io/api',
+    baseURL: 'https://knit.lightrains.com/api',
+    //baseURL: 'https://62d9a6f01dcd.ngrok.io/api',
     headers: {
         'Content-type': 'application/json'
     }
