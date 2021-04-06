2939fb679c5b475fa624b360a46e568a7ffbf1c7
niksmac <nikhil@lightrains.com>
2021-04-06 19:13:09 +0530

    Fix ui

---
+++ src/Footer.js
@@ -0,0 +1,20 @@
+import * as React from 'react'
+
+import { Container, Menu } from 'semantic-ui-react'
+import { NavLink } from 'react-router-dom'
+
+const Footer = () => {
+    return (
+        <Container>
+            <Menu text inverted>
+                <Menu.Item header as={NavLink} exact to="/" children="Home" />
+            </Menu>
+            <p className="footer text">
+                Unlocking Full Potential of DeFi Using Cross Chain Synthetics
+                and Bridges.
+            </p>
+        </Container>
+    )
+}
+
+export default Footer
