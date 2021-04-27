ea239e5ce4136d925c1c66851f09c22a6e31245a
knitmac <nikhilmohan@primetrade.ai>
2021-04-27 18:49:45 +0530

    Ref: lint

--- src/context/index.js
+++ src/context/index.js
@@ -1,11 +1,12 @@
-import React, { useState, createContext } from 'react'
+import React, { createContext, useState } from 'react'
+
 import { reactLocalStorage } from 'reactjs-localstorage'
 
 // Create Context Object
 export const CounterContext = createContext()
 
 // Create a provider for components to consume and subscribe to changes
-export const CounterContextProvider = props => {
+export const CounterContextProvider = (props) => {
     const [connectWallet, setConnectWallet] = useState(
         reactLocalStorage.get('connectWallet', null)
     )
